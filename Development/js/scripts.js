a037e0842e02f23449af98ec64eb72c45ded371e
mustaphography <mustaphography@gmail.com>
2020-06-27 20:15:07 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
 // 1
-
+const name = "Mustafa";
 // 2
-
+let age = 18;
 // 3 and 4
-
+console.log("انا" + " " + name + "و عمري" + " " + age);
 // 5 and 6
-
+console.log(age * 5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = ["photography", "programming", "videography", "swimming"];
 // 2
-
+hobbies.push(18);
+hobbies.push(18);
 // 3
-
+hobbies.pop();
 // 4
-
+let object = {
+  path: "web-design",
+  favorite: "CSS",
+  teachers: ["Moudhi", "Hussain", "MoMo 5a5a", "Rahaf", "Nancy"],
+};
 // 5
 
 // 6
 
 // 7
-
+console.log(object.teachers[0]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
